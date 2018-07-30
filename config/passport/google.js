5f1d79148e864c0547622f5ca70ffe78f69dd990
Tushar Sachde <tushar.sachde@sptr.co>
2018-07-30 17:22:42 +0530

    Google passport

--- config/passport/google.js
+++ config/passport/google.js
@@ -1,6 +1,6 @@
 global["GoogleKey"] = "AIzaSyDMgwXi6D38isibUfShc9C2mJyaHZZ2LpE";
-global["GoogleclientId"] = "1066475903870-6in3bmgspj576st44uno0joasig8dtbv.apps.googleusercontent.com";
-global["GoogleclientSecret"] = "-DqF0lFQf7kjGjby-vaW-aWs";
+global["GoogleclientId"] = "1066475903870-va8m2r3n3einp2qour7f49tj4ipqdgkr.apps.googleusercontent.com";
+global["GoogleclientSecret"] = "2mGD9knX54f4tihPEvlFfswK";
 
 passport.use(new GoogleStrategy({
         clientId: GoogleclientId,
