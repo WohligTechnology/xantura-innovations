858068fce2794944a3d031ddbb0ea2ff16837dab
Tushar Sachde <tushar.sachde@sptr.co>
2018-07-30 15:50:46 +0530

    Port 96

--- config/env/production.js
+++ config/env/production.js
@@ -25,7 +25,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 95,
+  port: 96,
   realHost: "https://xantura.sptr.co",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com", "sohan@wohlig.com"]
 
